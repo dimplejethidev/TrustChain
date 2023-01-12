da4a5154c2b7a677cdee52033e45f12cf38db8b9
Neel <neelp0980@gmail.com>
2023-01-12 10:05:45 +0530

    Resolved conflicts

--- src/components/form-elements/input.tsx
+++ src/components/form-elements/input.tsx
@@ -28,8 +28,9 @@ const Input = ({
         name={name}
         onChange={onChange}
         variant="outline"
-        className="mt-0 text-gray-700 dark:text-white"
-        focusBorderColor="#008dff"
+        className="mt-0 text-gray-800 shadow-sm placeholder:text-gray-500 dark:text-white"
+        borderColor={'gray.500'}
+        focusBorderColor="#a13bf7"
         placeholder={placeholder}
         type={type}
       />
